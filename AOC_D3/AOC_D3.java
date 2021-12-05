6bbe4626ffd9ce2f38c6a8a5f4655cb086f0fd56
jmkraut <jacobmatthewkraut@hotmail.com>
2021-12-06 00:30:13 +1000

    day 3 works correctly now, would like to tidy up the solution.

--- AOC_D3/AOC_D3.java
+++ AOC_D3/AOC_D3.java
@@ -5,19 +5,31 @@ import AdventOfCode.Helpers;
 
 public class AOC_D3 {
     public static void main(String[] args) throws IOException {
-        ArrayList<String> list = Helpers.GenerateArrayListFromFile("AdventOfCode\\AOC_D3\\dataset_test2.txt");
+        ArrayList<String> list = Helpers.GenerateArrayListFromFile("AdventOfCode\\AOC_D3\\dataset.txt");
         char[][] main = Helpers.ConvertStringArrayListTo2DCharArray(list, list.size(), list.get(0).length());
 
         String gamma = "";
         String epsilon = "";
+        int rowLength = main[0].length; // Stops out of bound 
 
-        for(int row = 0; row < main[row].length; row++) {
+        for(int row = 0; row < rowLength; row++) {
+            int bit1Counter = 0;
+            int bit0Counter = 0;
             for(int col = 0; col < main.length; col++) {
-                System.out.println(main[col][row]);
+                if(main[col][row] == '1') {
+                    bit1Counter++;
+                } else {
+                    bit0Counter++;
+                }
+            }
+            if(bit1Counter >= bit0Counter) {
+                gamma += 1;
+                epsilon += 0;
+            } else {
+                gamma += 0;
+                epsilon += 1;
             }
         }
-
-        System.out.println(gamma);
-        System.out.println(epsilon);
+        System.out.println(Integer.parseInt(gamma, 3) * Integer.parseInt(epsilon, 2));
     }
 }
